9150db8071f5363b852c2ed9ab2044f41dc93429
Arshadsheikh1 <raajarsh6@gmail.com>
2026-01-01 19:21:08 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -73,14 +73,13 @@ function Footer() {
                   raajarsh6@gmail.com
                 </a>
               </li>
-              <li><p className="text-[#FF073A] text-sm tesxt center"> Developed by Er Arshad sheikh</p></li>
             </ul>
           </div>
         </div>
 
         <div className="border-t border-gray-800 pt-8">
           <div className="flex flex-col md:flex-row justify-between items-center gap-4">
-            <p className="text-gray-400 text-sm"> Developed by
+            <p className="text-[#FF073A]"> Developed by
              <a href="https://www.instagram.com/sheikh_arshad.143/" target="_blank">
              Er Arshad sheikh</a>
             </p>
